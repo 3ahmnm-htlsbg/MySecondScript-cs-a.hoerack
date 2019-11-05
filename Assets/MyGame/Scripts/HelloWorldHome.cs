79dab8eefdd71b0bc168b446fbcc5698693e0d44
ahoerack03 <anna.hoerack2003@gmail.com>
2019-11-05 14:38:33 +0100

    update readme

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HelloWorld : MonoBehaviour
+public class HelloWorldHome : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(“Hello World at home“); 
+            Debug.Log("HelloWorldHome");
     }
 
     // Update is called once per frame
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MySecondScript-cs-a.hoerack
+Development platform
+Description: 2D project, practicing create unity project locally and on GitHub, coding first Debug message in Hello World script.
+Target platform: WebGL (refRes: 1280x720)
+Screenshot: TODO
+Game Control: TODO
+Third Party Material: TODO
+Limitations: TODO
+Lessons Learned: TODO
+
+Copyright by a.hoerack
